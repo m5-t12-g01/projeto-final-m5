9f43bab6bee184958b6c0db73428d18f85bf7f39
helena <heju1958@outlook.com>
2023-01-05 11:53:58 -0300

    feat: diary view

--- diaries/permissions.py
+++ diaries/permissions.py
@@ -1,11 +1,10 @@
 from rest_framework import permissions
-from users.models import User
+from diaries.models import Diary
 from rest_framework.views import View, Request
 
-
 class IsPermissionDiary(permissions.BasePermission):
-    def has_object_permission(self, request: Request, view: View, obj: User):
-        if request.user.is_authenticated and obj == request.user:
+    def has_object_permission(self, request: Request, view: View, obj: Diary):
+        if request.user.is_authenticated and obj.user == request.user:
             return True
         return False
 
--- diaries/views.py
+++ diaries/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Diary
 from .serializers import DiarySerializer
 from rest_framework.permissions import IsAuthenticated
-
+from .permissions import IsPermissionDiary
 
 class DiaryView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -16,3 +16,13 @@ class DiaryView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
+    
+        
+class DiaryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsPermissionDiary]
+
+    serializer_class = DiarySerializer
+    queryset = Diary.objects.all() 
+    
+    lookup_url_kwarg = "pk"    
